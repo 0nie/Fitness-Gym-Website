0684d148d43000bf85cb92627fd18fda5dd9b33d
Ronnie Valenzuela <onie.valenzuelaaa02@gmail.com>
2023-12-16 01:32:26 +0800

    first commit

--- main.js
+++ main.js
@@ -96,6 +96,29 @@ function redirectToContactPage() {
     window.location.href = 'contact.html';
 }
 
+// Cursor
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function(e){
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    // cursorOutline.style.left = `${posX}px`;
+    // cursorOutline.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`,
+
+    }, {duration: 500, fill: "forwards"})
+})
+
+
+
 //the image will appear from bottom its like respawning from below
 const scrollRevealOption = {
     distance: "100px",
